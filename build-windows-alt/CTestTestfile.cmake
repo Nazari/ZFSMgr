b9a7785bdfe5d6e3531aab7891314e991ff256b9
linarese <linarese@local>
2026-03-31 20:24:21 +0200

    Borrar build dir

--- build-windows-alt/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/eladi/work/ZFSMgr/resources
-# Build directory: C:/Users/eladi/work/ZFSMgr/build-windows-alt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[zfsmgr_helpers_test]=] "C:/Users/eladi/work/ZFSMgr/build-windows-alt/zfsmgr_helpers_test.exe")
-set_tests_properties([=[zfsmgr_helpers_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/eladi/work/ZFSMgr/resources/CMakeLists.txt;96;add_test;C:/Users/eladi/work/ZFSMgr/resources/CMakeLists.txt;0;")
